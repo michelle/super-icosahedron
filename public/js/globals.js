b15b0e9da6a0be2d688352d8f09ce827d4edd42b
Michelle Bu <michelle@michellebu.com>
2013-04-06 14:54:03 -0700

    globals

--- public/js/globals.js
+++ public/js/globals.js
@@ -1,6 +1,9 @@
 /** ====== GLOBALS ====== */
 
 // TODO: change these to be more reasonable.
+
+Globals = {};
+
 Globals.INNER_RADIUS = 50;
 Globals.OUTER_RADIUS = 100;
 Globals.TAU = (1 + Math.sqrt(5)) / 2;
