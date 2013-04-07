9ec96ac3f1be455c830ea2da7e3b5b1ab1215ba4
Michelle Bu <michelle@michellebu.com>
2013-04-07 08:17:49 -0700

    immunity

--- public/js/game.js
+++ public/js/game.js
@@ -137,7 +137,8 @@ Game.prototype.updateScore = function() {
 };
 
 Game.prototype.end = function() {
-  if (this.started) {
+  // 2 second immunity;
+  if (this.started && this.score > 2) {
     this.user.die(this.score);
     clearInterval(this.i0);
     clearTimeout(this.t0);
